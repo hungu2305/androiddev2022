94975f893435329894a67f6521bb92933fed7a4f
unknown <hungloveuyen235@gmail.com>
2021-10-28 22:34:18 +0700

    practise labwork14

--- USTHWeather/app/src/main/java/vn/edu/usth/weather/MyAsyncTask.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/MyAsyncTask.java
@@ -17,8 +17,6 @@ public class MyAsyncTask extends AsyncTask<Void, Integer,Void> {
     @Override
     protected void onPreExecute() {
         super.onPreExecute();
-        //Hàm này sẽ chạy đầu tiên khi AsyncTask này được gọi
-        //Ở đây mình sẽ thông báo quá trình load bắt đâu "Start"
         Toast.makeText(contextParent, "Start", Toast.LENGTH_SHORT).show();
     }
 
@@ -34,15 +32,13 @@ public class MyAsyncTask extends AsyncTask<Void, Integer,Void> {
 
     @Override
     protected void onProgressUpdate(Integer... values) {
-        //Hàm thực hiện update giao diện khi có dữ liệu từ hàm doInBackground gửi xuống
+
         super.onProgressUpdate(values);
 
     }
     @Override
     protected void onPostExecute(Void aVoid) {
         super.onPostExecute(aVoid);
-        //Hàm này được thực hiện khi tiến trình kết thúc
-        //Ở đây mình thông báo là đã "Finshed" để người dùng biết
         Toast.makeText(contextParent, "Network connected", Toast.LENGTH_SHORT).show();
     }
 }
